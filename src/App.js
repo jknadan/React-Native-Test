332d90e101346a995e5dd6369bfaf9be8ce65f4b
jknadan <jknadan@gmail.com>
2022-06-23 14:34:57 +0900

    [Day 3] : State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Fragment} from 'react';
-import {View, StyleSheet, Text, StatusBar, Button} from 'react-native';
+import {View, StyleSheet, Text, StatusBar, Button, Section} from 'react-native';
 import MyButton from "./components/MyButton";
+import Counter from "./components/counter";
+
 // 부모 컴포넌트 App.js
 const App = () => {
 
@@ -17,11 +19,11 @@ const App = () => {
       {/* 자식 컴포넌트 호출 My Button.js */}
       {/* 태그에 children 값을 추가하고 커스텀한 컴포넌트에 children 값을 출력하도록 해주면 되겠구마 */}
       <MyButton onPress={()=>alert('부모 컴포넌트에서 click')}> </MyButton>
-
-
+      <Counter/>
       <StatusBar style='auto'/>
 
     </View>
+
   );
 }
 
---
+++ src/components/counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import MyButton from "./MyButton";
+
+const Counter = () => {
+  const [ count , setCount ] = useState(0);
+  console.log(setCount);
+  return (
+    <View style={{
+      alignItems : 'center',
+    }}>
+      <Text style={{fontSize: 30, margin: 10}}>{count}</Text>
+      {/* setCount(count + 1) ===> count = count + 1 */}
+      <MyButton title="+1" onPress={() => setCount(count+1)}> </MyButton>
+      <MyButton title="-1" onPress={() => setCount(count-1)}> </MyButton>
+    </View>
+  );
+
+};
+
+export default Counter;
